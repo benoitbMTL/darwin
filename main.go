91f617a3aae2851c3592c1739a2cb8c7d88a36b5
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-21 21:35:44 -0400

    new index

--- main.go
+++ main.go
@@ -1,43 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
+package main
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Darwin Project</title>
-</head>
+import (
+	"net/http"
+	"os/exec"
+	"strings"
 
-<body>
-    <h1>Darwin Project</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.</p>
+	"github.com/labstack/echo/v4"
+)
 
-    <form id="ping-form" action="/ping" method="post">
-        <label for="ip-fqdn">Enter IP Address or FQDN:</label>
-        <input type="text" id="ip-fqdn" name="ip-fqdn" required>
-        <button type="submit">Ping</button>
-    </form>
+func main() {
+	e := echo.New()
 
-    <div id="ping-result"></div>
+	// Set the location of the templates directory
+	e.Static("/", "templates")
 
-    <script>
-        document.getElementById('ping-form').addEventListener('submit', function(event) {
-            event.preventDefault();
-            var ipFqdn = document.getElementById('ip-fqdn').value;
-            fetch('/ping', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: 'ip-fqdn=' + encodeURIComponent(ipFqdn)
-            })
-            .then(response => response.text())
-            .then(result => {
-                document.getElementById('ping-result').innerText = result;
-            })
-            .catch(error => console.error('Error:', error));
-        });
-    </script>
-</body>
+	// Route to render the HTML page
+	e.GET("/", func(c echo.Context) error {
+		return c.File("templates/index.html")
+	})
 
-</html>
+	// Route to handle the form submission
+	e.POST("/ping", func(c echo.Context) error {
+		ipFqdn := c.FormValue("ip-fqdn")
+
+		// Sanitize the input to avoid command injection
+		if strings.ContainsAny(ipFqdn, ";&|") {
+			return c.String(http.StatusBadRequest, "Invalid characters in input")
+		}
+
+		// Execute the ping command
+		cmd := exec.Command("ping", "-c", "4", ipFqdn)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		// Return the output of the ping command
+		return c.String(http.StatusOK, string(output))
+	})
+
+	// Start the server
+	e.Start(":8080")
+}
