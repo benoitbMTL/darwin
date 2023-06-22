e58bead6e5b8800f1afedf02b89aa1b8dbc38f4d
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-21 23:59:04 -0400

    command injection

---
+++ config.go
@@ -0,0 +1,13 @@
+package main
+
+const (
+	DVWA_URL       = "https://192.168.4.10"
+	DVWA_HOST      = "192.168.4.10"
+	SHOP_URL       = "https://shop.corp.fabriclab.ca"
+	FWB_URL        = "https://192.168.4.10/fwb/"
+	SPEEDTEST_URL  = "http://speedtest.corp.fabriclab.ca"
+	KALI_URL       = "https://flbr1kali01.fortiweb.fabriclab.ca"
+	TOKEN          = "eyJ1c2VybmFtZSI6InVzZXJhcGkiLCJwYXNzd29yZCI6ImZhY2VMT0NLeWFybjY3ISJ9Cg=="
+	FWB_MGT_IP     = "192.168.4.2"
+	POLICY         = "DVWA_POLICY"
+)
--- main.go
+++ main.go
@@ -22,7 +22,11 @@ func main() {
 		return c.File("templates/index.html")
 	})
 
-	// Route to handle the form submission
+	////////////////////////////////////////////////////////////////////////
+	// ACTION #1 PING                                                     //
+	////////////////////////////////////////////////////////////////////////
+
+	// Route to handle the ping form submission
 	e.POST("/ping", func(c echo.Context) error {
 		ipFqdn := c.FormValue("ip-fqdn")
 
@@ -41,6 +45,54 @@ func main() {
 		// Return the output of the ping command
 		return c.String(http.StatusOK, string(output))
 	})
+
+	////////////////////////////////////////////////////////////////////////
+	// ACTION #2 COMMAND INJECTION                                        //
+	////////////////////////////////////////////////////////////////////////
+
+	// Route to handle the command injection action
+	e.POST("/command-injection", func(c echo.Context) error {
+		username := c.FormValue("username")
+
+		// Map usernames to passwords
+		userPassMap := map[string]string{
+			"admin":   "password",
+			"gordonb": "abc123",
+			"1337":    "charley",
+			"pablo":   "letmein",
+			"smithy":  "password",
+		}
+
+		password, ok := userPassMap[username]
+		if !ok {
+			return c.String(http.StatusBadRequest, "Invalid username")
+		}
+
+		// Execute the curl command
+		cmd := exec.Command("curl", "https://192.168.4.10/login.php",
+			"-H", "authority: 192.168.4.10",
+			"-H", "cache-control: max-age=0",
+			"-H", "content-type: application/x-www-form-urlencoded",
+			"-H", "origin: https://192.168.4.10",
+			"-H", "referer: https://192.168.4.10/",
+			"-H", "user-agent: FortiWeb Demo Script",
+			"--insecure",
+			"--data-raw", "username="+username+"&password="+password+"&Login=Login",
+			"-c", "cookie.txt",
+		)
+
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		// Return the output of the curl command
+		return c.String(http.StatusOK, string(output))
+	})
+
+	////////////////////////////////////////////////////////////////////////
+	// START THE SERVER                                                   //
+	////////////////////////////////////////////////////////////////////////
 
 	// Start the server
 	e.Start(":8080")
