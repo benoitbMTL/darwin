1eb8c65c7149ed50e8c985e61c54f44d5853f3db
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-22 23:32:09 -0400

    dark logo

--- static/js/dark-mode-toggle.js
+++ static/js/dark-mode-toggle.js
@@ -1,9 +1,13 @@
 // DARK MODE TOGGLE
 
 document.getElementById('darkModeSwitch').addEventListener('change', function () {
+    var logoElement = document.getElementById('fortiweb-logo');
+
     if (this.checked) {
         document.body.classList.add('dark-mode');
+        logoElement.src = "/static/images/icon-fortiweb-dark.svg"; // Set the dark mode logo
     } else {
         document.body.classList.remove('dark-mode');
+        logoElement.src = "/static/images/icon-fortiweb.svg"; // Set the light mode logo
     }
 });
