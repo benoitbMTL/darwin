7b346f35badd2733dd4a5d2d7c5526dedb597c84
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-23 03:39:11 -0400

    button color

--- static/js/categories.js
+++ static/js/categories.js
@@ -7,4 +7,23 @@ function showCategory(categoryId) {
 
     // Show the selected category
     document.getElementById(categoryId).style.display = 'block';
+
+    // Get all buttons
+    var buttons = document.querySelectorAll('.category-buttons .btn');
+
+    // Loop through all buttons
+    for (var i = 0; i < buttons.length; i++) {
+        var button = buttons[i];
+
+        // If the button's onclick attribute matches the selected category, make it active
+        if (button.getAttribute('onclick') === "showCategory('" + categoryId + "')") {
+            button.classList.add('active');
+            button.setAttribute('aria-pressed', 'true');
+        }
+        // Otherwise, make it not active
+        else {
+            button.classList.remove('active');
+            button.setAttribute('aria-pressed', 'false');
+        }
+    }
 }
