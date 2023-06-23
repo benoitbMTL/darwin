793e9e7a44286ab5b0b3f09b1087c7bf9993c24a
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-22 20:07:38 -0400

    debug ping

--- actions.go
+++ actions.go
@@ -20,63 +20,71 @@ func registerActions(e *echo.Echo) {
 }
 
 func handlePingAction(c echo.Context) error {
-    ipFqdn := c.FormValue("ip-fqdn")
+	ipFqdn := c.QueryParam("ip-fqdn") // get ipFqdn from query parameters
 
-    // Sanitize the input to avoid command injection
-    if strings.ContainsAny(ipFqdn, ";&|") {
-        return c.String(http.StatusBadRequest, "Invalid characters in input")
-    }
+	// Sanitize the input to avoid command injection
+	if strings.ContainsAny(ipFqdn, ";&|") {
+		return c.String(http.StatusBadRequest, "Invalid characters in input")
+	}
 
-    // Create a new command for ping
-    cmd := exec.Command("ping", "-c", "4", ipFqdn)
+	// Create a new command for ping
+	cmd := exec.Command("ping", "-c", "4", ipFqdn)
 
-    // Create a pipe to capture the command output
-    stdout, err := cmd.StdoutPipe()
-    if err != nil {
-        return c.String(http.StatusInternalServerError, err.Error())
-    }
+	// Create a pipe to capture the command output
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
 
-    // Create a reader for command output
-    reader := bufio.NewReader(stdout)
+	// Create a reader for command output
+	reader := bufio.NewReader(stdout)
 
-    // Start the command
-    if err := cmd.Start(); err != nil {
-        return c.String(http.StatusInternalServerError, err.Error())
-    }
+	// Start the command
+	if err := cmd.Start(); err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
 
-    c.Response().Header().Set("Content-Type", "text/event-stream")
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextEventStream)
 
-    // Create a goroutine to read and send the command output
-    go func() {
-        defer stdout.Close()
+	// Create a goroutine to read and send the command output
+	go func() {
+		defer stdout.Close()
 
-        // While command is running
-        for {
-            // Read line from the command's output
-            line, _, err := reader.ReadLine()
-            if err != nil {
-                if err == io.EOF {
-                    break
-                }
-                // log the error
-                return
-            }
+		// Check if the connection is closed by the client
+		notify := c.Response().CloseNotify()
 
-            // Write the output to SSE
-            c.Response().Write([]byte(fmt.Sprintf("data: %s\n\n", line)))
+		// While command is running
+		for {
+			select {
+			case <-notify: // if the client has closed the connection
+				cmd.Process.Kill() // kill the ping command
+				return
+			default:
+				// Read line from the command's output
+				line, _, err := reader.ReadLine()
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+					// log the error
+					return
+				}
 
-            c.Response().Flush() // Send it immediately
-        }
+				// Write the output to SSE
+				c.Response().Write([]byte(fmt.Sprintf("data: %s\n\n", line)))
 
-        // Wait for the command to complete
-        if err := cmd.Wait(); err != nil {
-            // log the error
-        }
-    }()
+				c.Response().Flush() // Send it immediately
+			}
+		}
 
-    return c.NoContent(http.StatusOK)
+		// Wait for the command to complete
+		if err := cmd.Wait(); err != nil {
+			// log the error
+		}
+	}()
+
+	return c.NoContent(http.StatusOK)
 }
-
 
 func handleCommandInjectionAction(c echo.Context) error {
 	username := c.FormValue("username")
