f27a82c61f82059b0a4a94786e0900f3cf26df0d
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-23 13:44:50 -0400

    tile dark mode

--- actions.go
+++ actions.go
@@ -194,7 +194,7 @@ func handlePingAction(c echo.Context) error {
 	}
 
 	// Execute the ping command
-	cmd := exec.Command("ping", "-c", "4", ipFqdn)
+	cmd := exec.Command("ping", "-c", "1", ipFqdn)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return c.String(http.StatusInternalServerError, err.Error())
