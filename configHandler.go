1b6df3f13b4dcf2029bb3fa64606142f6b00a341
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-23 18:11:14 -0400

    DefaultConfigHandler

--- configHandler.go
+++ configHandler.go
@@ -19,26 +19,32 @@ type Config struct {
 	UserPassMap   map[string]string `json:"user_pass_map"`
 }
 
-func ConfigHandler(c echo.Context) error {
-	config := Config{
-		DVWA_URL:      "https://192.168.4.10",
-		DVWA_HOST:     "192.168.4.10",
-		SHOP_URL:      "https://shop.corp.fabriclab.ca",
-		FWB_URL:       "https://192.168.4.10/fwb/",
-		SPEEDTEST_URL: "http://speedtest.corp.fabriclab.ca",
-		KALI_URL:      "https://flbr1kali01.fortiweb.fabriclab.ca",
-		TOKEN:         "eyJ1c2VybmFtZSI6InVzZXJhcGkiLCJwYXNzd29yZCI6ImZhY2VMT0NLeWFybjY3ISJ9Cg==",
-		FWB_MGT_IP:    "192.168.4.2",
-		POLICY:        "DVWA_POLICY",
-		USER_AGENT:    "FortiWeb Demo Tool",
-		UserPassMap: map[string]string{
-			"admin":   "password",
-			"gordonb": "abc123",
-			"1337":    "charley",
-			"pablo":   "letmein",
-			"smithy":  "password",
-		},
-	}
+var defaultConfig = Config{
+	DVWA_URL:      "https://192.168.4.10",
+	DVWA_HOST:     "192.168.4.10",
+	SHOP_URL:      "https://shop.corp.fabriclab.ca",
+	FWB_URL:       "https://192.168.4.10/fwb/",
+	SPEEDTEST_URL: "http://speedtest.corp.fabriclab.ca",
+	KALI_URL:      "https://flbr1kali01.fortiweb.fabriclab.ca",
+	TOKEN:         "eyJ1c2VybmFtZSI6InVzZXJhcGkiLCJwYXNzd29yZCI6ImZhY2VMT0NLeWFybjY3ISJ9Cg==",
+	FWB_MGT_IP:    "192.168.4.2",
+	POLICY:        "DVWA_POLICY",
+	USER_AGENT:    "FortiWeb Demo Tool",
+	UserPassMap: map[string]string{
+		"admin":   "password",
+		"gordonb": "abc123",
+		"1337":    "charley",
+		"pablo":   "letmein",
+		"smithy":  "password",
+	},
+}
 
-	return c.JSON(http.StatusOK, config)
+func ConfigHandler(c echo.Context) error {
+	// Here you would normally retrieve the current configuration from where you have it stored
+	// For this example, we'll just return the default configuration
+	return c.JSON(http.StatusOK, defaultConfig)
+}
+
+func DefaultConfigHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, defaultConfig)
 }
