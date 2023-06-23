6f51cc90f1b5312fa2e6eeea2dbe106e610cc27f
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-22 23:40:32 -0400

    New Dark Mode Toggle

--- static/js/dark-mode-toggle.js
+++ static/js/dark-mode-toggle.js
@@ -1,17 +1,18 @@
-// DARK MODE TOGGLE
+document.addEventListener("DOMContentLoaded", function () {
+    // DARK MODE TOGGLE
+    document.getElementById('darkModeSwitch').addEventListener('change', function () {
+        var logoElement = document.getElementById('fortiweb-logo');
 
-document.getElementById('darkModeSwitch').addEventListener('change', function () {
-    var logoElement = document.getElementById('fortiweb-logo');
+        console.log("Dark mode toggle triggered"); // Log to console
 
-    console.log("Dark mode toggle triggered"); // Log to console
-
-    if (this.checked) {
-        console.log("Switching to dark mode"); // Log to console
-        document.body.classList.add('dark-mode');
-        logoElement.src = "/static/images/icon-fortiweb-dark.svg"; // Set the dark mode logo
-    } else {
-        console.log("Switching to light mode"); // Log to console
-        document.body.classList.remove('dark-mode');
-        logoElement.src = "/static/images/icon-fortiweb.svg"; // Set the light mode logo
-    }
+        if (this.checked) {
+            console.log("Switching to dark mode"); // Log to console
+            document.body.classList.add('dark-mode');
+            logoElement.src = "/static/images/icon-fortiweb-dark.svg"; // Set the dark mode logo
+        } else {
+            console.log("Switching to light mode"); // Log to console
+            document.body.classList.remove('dark-mode');
+            logoElement.src = "/static/images/icon-fortiweb.svg"; // Set the light mode logo
+        }
+    });
 });
