cc82f659de393c04e27fd602c0e37ecd4954e26a
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-22 23:37:03 -0400

    console.log

--- static/js/dark-mode-toggle.js
+++ static/js/dark-mode-toggle.js
@@ -3,10 +3,14 @@
 document.getElementById('darkModeSwitch').addEventListener('change', function () {
     var logoElement = document.getElementById('fortiweb-logo');
 
+    console.log("Dark mode toggle triggered"); // Log to console
+
     if (this.checked) {
+        console.log("Switching to dark mode"); // Log to console
         document.body.classList.add('dark-mode');
         logoElement.src = "/static/images/icon-fortiweb-dark.svg"; // Set the dark mode logo
     } else {
+        console.log("Switching to light mode"); // Log to console
         document.body.classList.remove('dark-mode');
         logoElement.src = "/static/images/icon-fortiweb.svg"; // Set the light mode logo
     }
