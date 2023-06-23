685d2c06127ab563bb2a6188351703b4ecf52cff
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-06-23 15:03:31 -0400

    tile dark mode

--- static/js/dark-mode-toggle.js
+++ static/js/dark-mode-toggle.js
@@ -2,15 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
     // DARK MODE TOGGLE
     document.getElementById('darkModeSwitch').addEventListener('change', function () {
         var logoElement = document.getElementById('fortiweb-logo');
+        var tiles = document.getElementsByClassName('tile');
         if (this.checked) {
             document.body.classList.add('dark-mode');
             if (logoElement) {
                 logoElement.src = "/static/images/icon-fortiweb-dark.svg"; // Set the dark mode logo
             }
+            // Add the dark-tile class to all tiles
+            for (var i = 0; i < tiles.length; i++) {
+                tiles[i].classList.add('dark-tile');
+            }
         } else {
             document.body.classList.remove('dark-mode');
             if (logoElement) {
                 logoElement.src = "/static/images/icon-fortiweb.svg"; // Set the light mode logo
+            }
+            // Remove the dark-tile class from all tiles
+            for (var i = 0; i < tiles.length; i++) {
+                tiles[i].classList.remove('dark-tile');
             }
         }
     });
