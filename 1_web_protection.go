a69f8b2dae9cd6dd731be29ec8ac8d43e2553de4
benoitmtl <81545589+benoitbMTL@users.noreply.github.com>
2023-12-15 22:37:30 -0500

    aa

--- 1_web_protection.go
+++ 1_web_protection.go
@@ -529,7 +529,7 @@ func handleNiktoWebScanAction(c echo.Context) error {
 	}
 
 	country := c.FormValue("country")
-	fmt.Println("Country:", country) // Debug Country
+	// fmt.Println("Country:", country) // Debug Country
 
 	nl := "\n"
 	cr := "\r"
